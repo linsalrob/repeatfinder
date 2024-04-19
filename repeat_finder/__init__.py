3ee21b3a84d92d9d7437727c4bf9cf95281d5767
linsalrob <raedwards@gmail.com>
2024-04-19 20:15:58 +0800

    making a module

--- repeat_finder/__init__.py
+++ repeat_finder/__init__.py
@@ -1,7 +1,7 @@
-from .cli.py import find_dna_repeats, repeats_in_fasta, run 
-from .colours.py import Colours, message
-from .rob_error.py import FastqFormatError, ColorNotFoundError
-from .sequences.py import stream_fastq, stream_fasta, stream_gfa_sequences
+from .cli import find_dna_repeats, repeats_in_fasta, run 
+from .colours import Colours, message
+from .rob_error import FastqFormatError, ColorNotFoundError
+from .sequences import stream_fastq, stream_fasta, stream_gfa_sequences
 
 __all__ = [
     'find_dna_repeats', 'repeats_in_fasta', 'run',
