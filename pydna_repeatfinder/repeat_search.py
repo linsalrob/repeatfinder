05ceaf9de295f55e74b77f24817454b3b53ec8ab
linsalrob <raedwards@gmail.com>
2024-06-12 10:10:01 +0930

    delinting

--- pydna_repeatfinder/repeat_search.py
+++ pydna_repeatfinder/repeat_search.py
@@ -6,6 +6,10 @@ __author__ = 'Rob Edwards'
 
 
 class RepeatSearch():
+    """
+    A repeat search class that allows us to pass data around conveniently
+    """
+
     def __init__(self, fastafile, minlen, gaplen, output):
         self.fastafile = fastafile
         self.minlen = minlen
@@ -14,7 +18,13 @@ class RepeatSearch():
         self.dnaseq = None
         self.dnaseqid = None
 
-    
+    def set_dna_sequence(seqid, sequence):
+        self.dnaseqid = seqid
+        self.dnaseq = sequence
 
-    
+    def get_dna_sequence():
+        return self.dnaseq
+
+    def get_seqid():
+        return self.dnaseqid
 
